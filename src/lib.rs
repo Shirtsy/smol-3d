6fc185ee067fea7ebf1cd1a1e813ee57ce5aca43
Shirtsy <monkeydew32@gmail.com>
2026-09-24 11:51:16 -0400

    Flatten pub API

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,16 @@
-pub mod geometry;
-pub mod traits;
+mod geometry;
+mod traits;
+
+pub use glam;
+
+pub use geometry::{
+    aabb::Aabb,
+    basis::{Basis, ToBasis},
+    bvh::Bvh,
+    mesh::{Mesh, MeshError},
+    ray::{HitRecord, Ray},
+    ray_grid::RayGrid,
+    tri::{Tri, WindingOrder},
+    unit_vec::{UnitVec3, UnitVecError},
+};
+pub use traits::{Bounded, Intersects};
